2c86325e960f0594609112b884e54f556000c3c8
Oliver Garvey <ogarvey@gmail.com>
2018-10-31 16:14:39 +0000

    Added basic app to demonstrate module exports/imports

---
+++ first-app/logger.js
@@ -0,0 +1,10 @@
+// Dummy URL
+var url = 'https://mylogger.io/log';
+
+function log(message) {
+    // Send an HTTP request to url
+    console.log('Message::',message)
+}
+
+// Add function to exports
+module.exports.log = log;
