40071f96f9e2f7fb8556bcc10d7ce3388df5799a
Oliver Garvey <ogarvey@gmail.com>
2018-10-31 16:51:21 +0000

    More os module examples

--- os/app.js
+++ os/app.js
@@ -2,5 +2,9 @@ const os = require("os");
 
 var totalMem = os.totalmem();
 var freeMem = os.freemem();
+var arch = os.arch();
+var cpus = os.cpus();
 
 console.log(`Your Total Memory is: ${totalMem}, with ${freeMem} free...`);
+console.log(`Your system is ${arch} based...`);
+cpus.forEach((cpu) => console.log('CPU info:::',cpu));
