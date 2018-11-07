63ef0438efd9692544d2664a7b153c3db80b4f61
Oliver Garvey <OGarvey@gmail.com>
2018-11-07 14:40:41 +0000

    Finished to express-demo app

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -36,6 +37,14 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+  
+  const { error } = validateCourse(req.body);
+
+  if (error) {
+    // Bad Request
+    res.status(400).send(error.details[0].message);
+  }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -43,6 +52,42 @@ app.post('/api/courses', (req, res) => {
   courses.push(course);
   res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+
+  if (!course) res.status(404).send(`Course with id: ${req.params.id} was not found...`);
+
+  const { error } = validateCourse(req.body)
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
+  }
+
+  course.name = req.body.name;
+
+  res.send(course);
+
+});
+
+app.delete('/api/courses/:id', (req,res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+
+  if (!course) res.status(404).send(`Course with id: ${req.params.id} was not found...`);
+
+  const index = courses.indexOf(course);
+  courses.splice(index,1);
+
+  res.send(course);
+})
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
